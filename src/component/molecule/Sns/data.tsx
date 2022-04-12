fa6fc08bb9e4cc1a6617cd1546aa91b6785632a1
niyato2001 <niyato2001@gmail.com>
2022-04-12 16:59:08 +0900

    SNS欄のテスト実装

---
+++ src/component/molecule/Sns/data.tsx
@@ -0,0 +1,11 @@
+import { SnsProps } from '.';
+
+export const storyList: { [key: string]: SnsProps } = {
+  default: {
+    facebook: { type: 'facebook' },
+    twitter: { type: 'twitter' },
+    line: { type: 'line' },
+    b: { type: 'b' },
+    pinterest: { type: 'pinterest' },
+  },
+};
