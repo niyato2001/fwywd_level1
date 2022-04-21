1e725a72f0a93f5f607db6358903ee391848f496
niyato2001 <niyato2001@gmail.com>
2022-04-21 10:30:58 +0900

    moleculeのSnsで下の余分な部分を削除するためにflex items-centerを追加

--- src/component/molecule/Sns/index.tsx
+++ src/component/molecule/Sns/index.tsx
@@ -10,19 +10,19 @@ export interface SnsProps {
 
 export const Sns: React.FC<SnsProps> = ({ facebook, twitter, line, b, pinterest }) => (
   <div className='flex gap-3'>
-    <button className='rounded' data-testid='sns-facebook'>
+    <button className='flex items-center rounded' data-testid='sns-facebook'>
       <Icon {...facebook} />
     </button>
-    <button className='rounded' data-testid='sns-twitter'>
+    <button className='flex items-center rounded' data-testid='sns-twitter'>
       <Icon {...twitter} />
     </button>
-    <button className='rounded' data-testid='sns-line'>
+    <button className='flex items-center rounded' data-testid='sns-line'>
       <Icon {...line} />
     </button>
-    <button className='rounded' data-testid='sns-b'>
+    <button className='flex items-center rounded' data-testid='sns-b'>
       <Icon {...b} />
     </button>
-    <button className='rounded' data-testid='sns-pinterest'>
+    <button className='flex items-center rounded' data-testid='sns-pinterest'>
       <Icon {...pinterest} />
     </button>
   </div>
