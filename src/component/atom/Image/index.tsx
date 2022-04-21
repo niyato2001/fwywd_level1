b70d79cd12992ef1f2efcd12e2cdd644c5b996e5
niyato2001 <niyato2001@gmail.com>
2022-04-22 00:09:39 +0900

    Imageにobjectfitとlayoutを追加

--- src/component/atom/Image/index.tsx
+++ src/component/atom/Image/index.tsx
@@ -5,8 +5,24 @@ export interface ImageProps {
   alt?: string;
   width?: number;
   height?: number;
+  objectFit?: 'contain' | 'cover' | 'fill' | 'none' | 'scale-down';
+  layout?: 'fill' | 'fixed' | 'intrinsic' | 'responsive';
 }
 
-export const Image: React.FC<ImageProps> = ({ src, alt = '', width, height }) => (
-  <NextImage src={src} alt={alt} width={width} height={height} />
+export const Image: React.FC<ImageProps> = ({
+  src,
+  alt = '',
+  width,
+  height,
+  objectFit,
+  layout,
+}) => (
+  <NextImage
+    src={src}
+    alt={alt}
+    width={width}
+    height={height}
+    objectFit={objectFit}
+    layout={layout}
+  />
 );
