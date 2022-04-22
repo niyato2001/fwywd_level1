24c1cf9eefe739d79d8f7cc0bb7dbb055c844360
niyato2001 <niyato2001@gmail.com>
2022-04-22 23:35:28 +0900

    SymapathyテストOK。見た目のみ未

--- src/component/template/Sympathy/Sympathy.test.tsx
+++ src/component/template/Sympathy/Sympathy.test.tsx
@@ -8,33 +8,21 @@ describe('component/template/Sympathy', () => {
       render(<SympathyPresenter {...storyList.default} />);
       expect(screen.getByTestId('sympathy-title')).toHaveTextContent(storyList.default.title);
     });
-    it('サブタイトルのテキストが表示されること', () => {
-      render(<SympathyPresenter {...storyList.default} />);
-      expect(screen.getByTestId('sympathy-subtitle')).toHaveTextContent(storyList.default.subtitle);
-    });
-    it('背景画像1が表示されること', () => {
-      render(<SympathyPresenter {...storyList.default} />);
-      expect(screen.getByRole('img', { name: 'bg1' })).toBeInTheDocument();
-    });
-    it('背景画像2が表示されること', () => {
-      render(<SympathyPresenter {...storyList.default} />);
-      expect(screen.getByRole('img', { name: 'bg2' })).toBeInTheDocument();
-    });
-    it('benefitCard1が表示されること', () => {
+    {
+      storyList.default.checkList.forEach((check, index) => {
+        it(`チェック${index}が表示されること`, () => {
+          render(<SympathyPresenter {...storyList.default} />);
+          expect(screen.getByTestId(`sympathy-check-${index}`)).toBeInTheDocument();
+        });
+      });
+    }
+    it('sympathyCard1が表示されること', () => {
       render(<SympathyPresenter {...storyList.default} />);
       expect(screen.getByTestId('sympathy-card-1')).toBeInTheDocument();
     });
-    it('benefitCard2が表示されること', () => {
+    it('sympathyCard2が表示されること', () => {
       render(<SympathyPresenter {...storyList.default} />);
       expect(screen.getByTestId('sympathy-card-2')).toBeInTheDocument();
-    });
-    it('テキスト1が表示されること', () => {
-      render(<SympathyPresenter {...storyList.default} />);
-      expect(screen.getByTestId('sympathy-text1')).toHaveTextContent(storyList.default.text1);
-    });
-    it('テキスト2が表示されること', () => {
-      render(<SympathyPresenter {...storyList.default} />);
-      expect(screen.getByTestId('sympathy-text2')).toHaveTextContent(storyList.default.text2);
     });
   });
 });
--- src/component/template/Sympathy/data.tsx
+++ src/component/template/Sympathy/data.tsx
@@ -4,25 +4,37 @@ import sympathyImage2 from '~/img/sympathy2.png';
 
 export const storyList: { [key: string]: SympathyProps } = {
   default: {
-    title: 'あながた本気でかわるために必要な2つの条件',
-    subtitle: '成果 = モチベーション × 能力',
+    title: '夢を叶える仲間が欲しい、あなたへ',
+    checkList: [
+      {
+        icon: { type: 'check', size: 36 },
+        text: '新しいことを始めたい気持ちは強い',
+        color: '800',
+      },
+      {
+        icon: { type: 'check', size: 36 },
+        text: '学びに必要な教材がたくさんあることは知っている',
+        color: '800',
+      },
+      {
+        icon: { type: 'check', size: 36 },
+        text: '最後のひと押しの勇気が欲しい',
+        color: '800',
+      },
+    ],
     sympathyCard1: {
-      word: '仲間',
-      icon: { type: 'arrow', size: 30 },
-      title: { type: 'default-h3', lg: 'モチベーション', base: '' },
-      image: { src: sympathyImage1, alt: 'sympathy1', height: 315, width: 315 },
-      text1: '熱量の高い',
-      text2: '最高の仲間と切磋琢磨',
+      tag: '悩み1',
+      title: { type: 'withbase', lg: '仲間', base: 'がいない' },
+      image: { src: sympathyImage1, alt: '', height: 315, width: 316 },
+      text1: '会社の同僚や友人と一緒に',
+      text2: '学ぶのは難しい',
     },
     sympathyCard2: {
-      word: '目的',
-      icon: { type: 'arrow', size: 30 },
-      title: { type: 'default-h3', lg: '能力', base: '' },
-      image: { src: sympathyImage2, alt: 'sympathy2', height: 315, width: 315 },
-      text1: 'プロレベルで必要な',
-      text2: '技術ロードマップを網羅',
+      tag: '悩み2',
+      title: { type: 'withbase', lg: '目的', base: 'があいまい' },
+      image: { src: sympathyImage2, alt: '', height: 315, width: 316 },
+      text1: 'ゆるい繋がりのコミュニティでは',
+      text2: '目的が達成できない',
     },
-    text1: '良き仲間と目的を持った学びができる環境を提供します。',
-    text2: '成果に繋がる学びがここにあります。',
   },
 };
