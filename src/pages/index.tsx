859483bae74e8082779c96bf5818ec3e4b86ae59
niyato2001 <niyato2001@gmail.com>
2022-04-25 10:20:39 +0900

    getServerSidePropsとaxiosでGGSの情報を取得しindex.tsxで反映

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import { ReactNode } from 'react';
 import { Benefit } from '@/component/template/Benefit';
 import { Cta } from '@/component/template/Cta';
 import { Hero } from '@/component/template/Hero';
@@ -6,10 +9,11 @@ import { PageHeader } from '@/component/template/PageHeader';
 import { Price } from '@/component/template/Price';
 import { Sympathy } from '@/component/template/Sympathy';
 
-const Home: React.FC = () => (
+const Home: React.FC<Props> = (props: Props) => (
   <>
     <PageHeader />
     <main className='max-w-full'>
+      <p>{props.num}</p>
       <Hero />
       <Sympathy />
       <Benefit />
@@ -21,3 +25,21 @@ const Home: React.FC = () => (
 );
 
 export default Home;
+
+interface Props {
+  num: number;
+  children?: ReactNode;
+}
+
+const gssUrl = 'https://api.steinhq.com/v1/storages/624e61c04906bb053738ccf4/sheet1';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await axios.get(gssUrl);
+  const props: Props = {
+    num: response.data.length,
+  };
+
+  return {
+    props: props,
+  };
+};
